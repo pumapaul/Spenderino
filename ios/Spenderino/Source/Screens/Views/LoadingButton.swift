8dcaf5a397c0433612bd482783f4e06c7801138c
Paul Weber <work@paul-weber.de>
2022-02-18 12:29:48 +0100

    fix lint

--- ios/Spenderino/Source/Screens/Views/LoadingButton.swift
+++ ios/Spenderino/Source/Screens/Views/LoadingButton.swift
@@ -11,7 +11,7 @@ struct LoadingButton<Label: View>: View {
     let isLoading: Bool
     let action: () -> Void
     let label: () -> Label
-    
+
     var body: some View {
         Button {
             action()
@@ -26,4 +26,3 @@ struct LoadingButton<Label: View>: View {
         }
     }
 }
-
